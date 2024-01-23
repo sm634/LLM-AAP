102f02a61c8736cdfde1b2e870cafc3dee0d4929
Safal Mukhia <safal.mukhia@ibm.com>
2024-01-23 14:54:17 +0000

    embeddings update
    Signed-off-by: Safal Mukhia <safal.mukhia@ibm.com>

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import yaml
 from src.article_classifier import run_article_classifier
+from src.embeddings import run_embeddings_comparison
 
 
 def main():
@@ -8,8 +9,13 @@ def main():
         config = yaml.safe_load(f)
 
     task = config['TASK'].lower()
+    print("Running task: ", task)
     if task == 'article_classifier':
         run_article_classifier()
+    elif task == 'embeddings_comparison':
+        run_embeddings_comparison()
+
+    print("Task Complete")
 
 
 if __name__ == '__main__':

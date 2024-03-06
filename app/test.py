2bd3493dd81c11628bbb10ba0cccb721b17f104e
Safal Mukhia <safal.mukhia@ibm.com>
2024-03-06 16:03:59 +0000

    complaints handler UI init
    Signed-off-by: Safal Mukhia <safal.mukhia@ibm.com>

--- app/test.py
+++ app/test.py
@@ -1,7 +1,16 @@
+import pandas as pd
 
-categories = "frequency, urgency, resolution, communication, fraud, update"
-print(type(categories))
+# Example multi-index series
+index = pd.MultiIndex.from_tuples([('A', 1), ('A', 2), ('B', 1), ('B', 2)])
+data = pd.Series([10, 20, 30, 40], index=index)
 
-categories = categories.split(',')
-print(categories)
+# Original multi-index series
+print("Original Series:")
+print(data)
+
+# Pivot the multi-index series
+pivot_data = data.unstack()
+print("\nPivoted DataFrame:")
+print(pivot_data)
+
 breakpoint()
